de5bfda6c16a08bd03f5d6b78af6d0b5fb3fef0c
Morvana Bonin <morvanabonin@gmail.com>
2021-09-10 16:03:37 -0300

    Update main.go

--- clean-string-mail/main.go
+++ clean-string-mail/main.go
@@ -9,16 +9,30 @@ import (
 func main() {
 	fmt.Println("Teste de limpeza")
 
-	//sDomain := "mail.mail.mail.mail.mail.mail.mail.cli.esrvvenancio.com.br."
-	sDomain := "mail.mail.mail.mail.mta-sts.5cde831a.cli.dnzpark.com.br."
+	qName := "mail.mail.mail.mail.mail.mail.mail.cli.esrvvenancio.com.br."
+	//qName := "mail.mail.mail.mail.mta-sts.5cde831a.cli.dnzpark.com.br."
 
-	if strings.Contains(sDomain, "mail.") {
-		rx := regexp.MustCompile(`mail.`)
-		matches := rx.FindAllString(sDomain, -1)
-		fmt.Println(matches)
-		mailString := strings.Join(matches, "")
-		//fmt.Println(mailString)
-		fmt.Println(strings.Replace(sDomain, mailString, "mail.", -1))
+	if strings.Contains(qName, "mail.") {
+		qName = cleanMailDomain(qName)
+		fmt.Println(qName)
 	}
 
+}
+
+// cleanMailDomain
+// Função criada para limpar os domínios que vem com vários "mail."
+func cleanMailDomain(domain string) string {
+	// regex apenas para passar o padrão mail,
+	// nosso interesse aqui é retornar todos
+	rx := regexp.MustCompile(`mail.`)
+	matches := rx.FindAllString(domain, -1)
+
+	// queremos a string inteira que veio em formato de slice
+	mailString := strings.Join(matches, "")
+
+	// com a string já obtendo a quantidade total de `mail`,
+	// nós substituímos tudo por apenas um `mail`
+	domainClean := strings.Replace(domain, mailString, "mail.", -1)
+
+	return domainClean
 }
