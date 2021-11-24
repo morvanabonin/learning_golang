80cc2ea2df1b598690b7d92113ca50c66b82c623
Morvana Bonin <canyslupus@gmail.com>
2021-11-24 14:26:55 -0300

    Tests with file
    Tests using os and io of Golang

---
+++ files/rename.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	// get path file
+	// create another file
+	// rename this file
+	// remove the old file
+
+	src := "abc/def/ghj/src.txt"
+
+	dir := filepath.Dir(src)
+
+	dst := dir + "/dst.txt"
+
+	fmt.Println(src)
+	fmt.Println(dst)
+
+	os.OpenFile(dst, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0755)
+
+	copy(src, dst)
+
+	if _, err := os.Stat(src); err != nil {
+		// The source does not exist or some other error accessing the source
+		log.Fatal("Error to check the source file:", err)
+	}
+	if _, err := os.Stat(dst); err != nil {
+		// The destination exists or some other error accessing the destination
+		log.Fatal("Erro to check the temporary file:", err)
+	}
+	if err := os.Rename(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func copy(src, dst string) error {
+
+	checkStatFile, err := os.Stat(src)
+
+	if err != nil {
+		return err
+	}
+
+	if !checkStatFile.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+
+	if err != nil {
+		return err
+	}
+
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+
+	if err != nil {
+		return err
+	}
+
+	defer destination.Close()
+
+	io.Copy(destination, source)
+
+	return nil
+}
